86f3bfaef69fab8750abcd4b70aa26b7bc17e64d
Shenwei <92427364+Shenwei625@users.noreply.github.com>
2022-06-13 10:07:51 +0800

    Update five_plot.sh
    Divide the pathogenic state into three categories (T, F, unknown), and plot each category

--- five_plot.sh
+++ five_plot.sh
@@ -1,9 +1,10 @@
 #! usr/bin/bash
 file1=$1
 file2=$2
+file3=$3
 
 # 对SIFT PolyPhen进行处理（保留括号里面的数字）
-for F in $1 $2; do 
+for F in $1 $2 $3; do 
     sed -i 's/#//g' $F
     tsv-select -H --fields Uploaded_variation,SIFT,PolyPhen $F > merge.tsv
     HEAD=$(head -n 1 merge.tsv)
@@ -23,13 +24,14 @@ rm merge.tsv
 # 每个参数做一个箱线图,致病与非致病作为对比放在一张图里
 for i in cDNA_position CDS_position Protein_position SIFT PolyPhen;do 
     echo -e "Making plot for $i\n"
-    for F in $1 $2; do 
+    for F in $1 $2 $3; do 
         file_name=$(echo $F | cut -d "." -f 1)
         tsv-filter -H --str-ne $i:- $F |
         tsv-select -H --fields Pathogenicity,$i > $file_name.$i.lst 
     done 
     sed '1d' F.$i.lst >> T.$i.lst
-    rm F.$i.lst
+    sed '1d' unknown.$i.lst >> T.$i.lst
+    rm F.$i.lst unknown.$i.lst
     mv T.$i.lst $i.lst
 
     #作图
