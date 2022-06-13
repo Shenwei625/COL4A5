889c63cd3f0094edc2ac12fab22b83fde815e388
Shenwei <92427364+Shenwei625@users.noreply.github.com>
2022-06-13 10:20:56 +0800

    Update AC_and_codons.sh
    Divide the pathogenic state into three categories (T, F, unknown), and plot each category

--- AC_and_codons.sh
+++ AC_and_codons.sh
@@ -1,9 +1,10 @@
 #! usr/bin/bash
 file1=$1
 file2=$2
+file3=$3
 
 # 获得绘图文件
-for F in $1 $2; do 
+for F in $1 $2 $3; do 
     file_name=$(echo $F | cut -d "." -f 1)
     for i in Amino_acids Codons; do
         tsv-filter -H --str-ne $i:- $F | 
@@ -50,10 +51,24 @@ Rscript -e '
         theme(text=element_text(size = 20, face = "bold")) +
         theme(legend.position = "none", plot.title=element_text(hjust=0.5)) +
         scale_fill_manual(values=samplecolor(colorcount))
+        
+        FILE3 <- paste(sep="","unknown.", J, ".tsv")
+        TREEDATA3 <- read.table(FILE3,header=TRUE,sep = "\t")
+        TITLE3 <- paste(sep="","unknown.", J)
+        colorcount = length(TREEDATA3[,1])
+        samplecolor=colorRampPalette(brewer.pal(12,"Paired"))
+        p3 <- ggplot(TREEDATA3, aes(area = count, fill = get(J), label = paste(get(J), count,sep = "," ),subgroup = get(J))) +
+        geom_treemap() +
+        geom_treemap_text(colour = "black",place = "bottomleft",size = 8) +
+        geom_treemap_subgroup_border(colour = "white", size = 2) +
+        labs(title = TITLE3) +
+        theme(text=element_text(size = 20, face = "bold")) +
+        theme(legend.position = "none", plot.title=element_text(hjust=0.5)) +
+        scale_fill_manual(values=samplecolor(colorcount))
 
-        p1 / p2
+        p1 / p2 /p3
         NAME <- paste(sep="", J, ".png" )
-        ggsave(file=NAME, width=12, height=10, dpi=300)
+        ggsave(file=NAME, width=12, height=15, dpi=300)
     }
 '
 
